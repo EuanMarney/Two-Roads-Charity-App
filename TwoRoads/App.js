6ab7228a969a0f7d1e6bb64a1a3e2814acfee91e
Tade5250 <2619191a@student.gla.ac.uk>
2023-11-08 12:54:50 +0000

    Temi changes to App.js

--- TwoRoads/App.js
+++ TwoRoads/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test! Cian - Test</Text>
+      <Text>test! Cian, Temi - Test</Text>
       <StatusBar style="auto" />
     </View>
   );
