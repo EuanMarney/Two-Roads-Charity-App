37cd13bc4bfba4159117d587bf1ce8f609f48a15
Patrick Kiehlmann <2623719k@student.gla.ac.uk>
2023-11-08 13:13:43 +0000

    added an image that forwards to a new page

--- TwoRoads/App.js
+++ TwoRoads/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, Button, Image, StyleSheet, TouchableHighlight } from 'react-native';
+import NewPage from './testpage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NewPage" component={NewPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  const handleImagePress = () => {
+    navigation.navigate('NewPage');
+  };
+
+  return (
     <View style={styles.container}>
-      <Text>test! Cian - Test</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight onPress={handleImagePress}>
+        <Image
+          style={styles.image}
+          source={require('./assets/twoRoads.png')}
+        />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -16,5 +40,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
   },
 });
