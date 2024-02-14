06a8ff6151cdc489345a34fb56056159c46a34d3
Tade5250 <2619191a@student.gla.ac.uk>
2024-02-14 13:22:51 +0000

    Added audio file to the assets page

--- TwoRoads/src/screens/DailyMindfulnessScreen.js
+++ TwoRoads/src/screens/DailyMindfulnessScreen.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { Text } from "react-native";
-import Layout from "../components/Layout";
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av';
+import Layout from '../components/Layout';
 
 const DailyMindfulnessScreen = () => {
+  const [sound, setSound] = useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+      require('../assets/audio/Mindfulness The Chocolate Meditation.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <Layout>
       <Text>This is the Daily Mindfulness page</Text>
+      <Button title="Play Sound" onPress={playSound} />
     </Layout>
   );
 };
