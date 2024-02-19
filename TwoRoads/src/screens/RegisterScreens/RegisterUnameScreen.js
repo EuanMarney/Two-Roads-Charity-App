87e88e2ca0b070843a933cdc8850a14f31d5c7f1
patokiehl <patrickkiehlmann@gmail.com>
2024-02-19 21:04:10 +0000

    ci(errors): removing errors in eslint ci/cd
    addressed the errors being spat out by the gitlab runner in RegisterUnameScreen.js

--- TwoRoads/src/screens/RegisterScreens/RegisterUnameScreen.js
+++ TwoRoads/src/screens/RegisterScreens/RegisterUnameScreen.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-import { View, TouchableOpacity, Text, TextInput, StyleSheet } from "react-native";
-import { StatusBar } from 'react-native';
 import * as SecureStore from 'expo-secure-store'
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, TextInput, StyleSheet, StatusBar } from "react-native";
 
 const RegisterScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
