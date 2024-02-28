298298a696d59979a024efe25e51fdcfc0accfb8
Tade5250 <2619191a@student.gla.ac.uk>
2024-02-28 11:05:39 +0000

    feat: introduce unit tests for Login Header component

--- TwoRoads/src/components/Header/LoginHeader.js
+++ TwoRoads/src/components/Header/LoginHeader.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import {View, Image} from 'react-native';
+import { View, Image } from 'react-native';
 
 import stylesheet from "../Styles/stylesheet";
 
 const LoginHeader = () => {
   return (
-    <View style={stylesheet.loginHeaderContainer}>
+    <View style={stylesheet.loginHeaderContainer} testID="login-header">
       <Image
         source={require("../../assets/twoRoads.png")}
         style={stylesheet.loginImage}
