64975e17d8ed0b5a78f497857fcc20770bac6b85
Tade5250 <2619191a@student.gla.ac.uk>
2024-02-28 12:04:50 +0000

    merging with main

--- TwoRoads/src/components/Header/LoginHeader.js
+++ TwoRoads/src/components/Header/LoginHeader.js
@@ -1,10 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-<<<<<<< HEAD
-import { View, Image } from 'react-native';
-=======
 import {View, Image} from 'react-native';
->>>>>>> 015b05748414344972ae95ea13ee852c5171ed6b
 
 import stylesheet from "../Styles/stylesheet";
 
